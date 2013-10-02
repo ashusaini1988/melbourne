ddf5ae4ded228438066590749e39a6006f8b7227
Ashish Saini <asain@deakin.edu.au>
2013-10-02 14:25:51 +1000

    Added useful comments to script

--- fibo.py
+++ fibo.py
@@ -1,9 +1,13 @@
 def fibonacci(n):
+    '''
+    this is a fibonacci series function.
+    '''
     a, b = 0, 1
     while n > 0:
         a, b = b, a+b
         n -= 1
     return a
+    
     
 
 #print fibonacci(1000)
@@ -14,3 +18,5 @@ assert fibonacci(1) == 1
 assert fibonacci(2) == 1
 assert fibonacci(3) == 2
 assert fibonacci(5) == 5
+
+
